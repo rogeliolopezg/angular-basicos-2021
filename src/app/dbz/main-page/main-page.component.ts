61d966085dc959bf06f311de3401da7727ab7b8a
Rogelio-Lopez <rogeliolopezg@yahoo.es>
2021-12-12 13:43:22 -0500

    segundo commit

---
+++ src/app/dbz/main-page/main-page.component.ts
@@ -0,0 +1,29 @@
+import { Component} from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
+
+
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html'
+})
+
+export class MainPageComponent {
+
+  personajes: Personaje[] = [
+    {
+      nombre: 'Goku',
+      poder: 15000
+    },
+    {
+      nombre: 'Veguetta',
+      poder: 7500
+    }
+  ]; 
+
+  nuevo : Personaje = {
+    nombre: 'Maestro Roshi',
+    poder: 1000
+  }
+
+}
